026079cfe673c47d79366ddcf816dcc3d12b425d
kasheemlew <417879751@qq.com>
2018-09-06 23:23:59 +0800

    add group-anagrams

---
+++ 49.group-anagrams/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        m = dict()
+        for str in strs:
+            sorted_str = ''.join(sorted(str))
+            if sorted_str not in m.keys():
+                m[sorted_str] = list()
+            m[sorted_str].append(str)
+        return list(m.values())
