4d2a68e0e3a0fabf4e46687988faec3bae0ddd64
kasheemlew <417879751@qq.com>
2019-02-16 09:14:53 +0800

    add 67 test

--- 67.add-binary/main_test.go
+++ 67.add-binary/main_test.go
@@ -21,3 +21,22 @@ func TestReverse(t *testing.T) {
 		}
 	}
 }
+
+func TestAddBinary(t *testing.T) {
+	tables := []struct {
+		a, b, op string
+	}{
+		{"11", "1", "100"},
+		{"1010", "1011", "10101"},
+		{"", "", ""},
+		{"1", "", "1"},
+		{"", "0", "0"},
+	}
+
+	for _, table := range tables {
+		if op := addBinary(table.a, table.b); op != table.op {
+			t.Errorf("addBinary(%s, %s) should return %s, but got %s",
+				table.a, table.b, table.op, op)
+		}
+	}
+}
