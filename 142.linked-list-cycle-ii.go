88916af5bd3a2f76da004a6e0c221235520c2406
klew <kasheemlew@mails.ccnu.edu.cn>
2019-05-09 23:11:31 +0800

    Update 142.linked-list-cycle-ii.go

--- 142.linked-list-cycle-ii.go
+++ 142.linked-list-cycle-ii.go
@@ -17,3 +17,29 @@ func detectCycle(head *ListNode) *ListNode {
 	}
 	return nil
 }
+
+func detectCycle2(head *ListNode) *ListNode {
+	// Using Floyd Detection
+	if head == nil {
+		return nil
+	}
+	p1, p2 := head, head
+	if p1.Next == nil || p2.Next == nil || p2.Next.Next == nil {
+		return nil
+	}
+	p1 = p1.Next
+	p2 = p2.Next.Next
+	for *p1 != *p2 {
+		if p1.Next == nil || p2.Next == nil || p2.Next.Next == nil {
+			return nil
+		}
+		p1 = p1.Next
+		p2 = p2.Next.Next
+	}
+	p1 = head
+	for *p1 != *p2 {
+		p1 = p1.Next
+		p2 = p2.Next
+	}
+	return p1
+}
