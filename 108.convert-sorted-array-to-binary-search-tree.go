434cb414bd1d2fda83a60dd39d1a22e601d137db
kasheemlew <417879751@qq.com>
2019-05-22 21:56:54 +0800

    add 108

---
+++ 108.convert-sorted-array-to-binary-search-tree.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+	return &TreeNode{
+		Val:   nums[len(nums)/2],
+		Left:  sortedArrayToBST(nums[0 : len(nums)/2]),
+		Right: sortedArrayToBST(nums[len(nums)/2+1 : len(nums)]),
+	}
+}
