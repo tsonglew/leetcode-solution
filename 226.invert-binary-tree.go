d7bbe27279bf1dc83c89286a825d66436445e260
kasheemlew <417879751@qq.com>
2019-05-27 11:09:08 +0800

    add 226

---
+++ 226.invert-binary-tree.go
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	root.Left = invertTree(root.Left)
+	root.Right = invertTree(root.Right)
+	root.Left, root.Right = root.Right, root.Left
+	return root
+}
