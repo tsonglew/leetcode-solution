efc9e8f30905789ce9106777f1429be25eb052e1
kasheemlew <417879751@qq.com>
2019-06-02 09:29:21 +0800

    add 112

---
+++ 112.path-sum.go
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func hasPathSum(root *TreeNode, sum int) bool {
+    if root == nil {
+        return false
+    }
+    if root.Left == nil && root.Right == nil && root.Val == sum {
+        return true
+    }
+    return hasPathSum(root.Left, sum - root.Val) || hasPathSum(root.Right, sum - root.Val)
+}
