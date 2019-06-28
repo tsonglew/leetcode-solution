5689b69d6406f011c392c635049e52f72b6a5779
kasheemlew <417879751@qq.com>
2019-06-28 09:48:23 +0800

    updated 437

--- 437.path-sum-iii.py
+++ 437.path-sum-iii.py
@@ -9,16 +9,19 @@ import copy
 class Solution:
     def pathSum(self, root: TreeNode, sum: int) -> int:
         self.cnt = 0
-        def canSum(root, num_set, total):
+        def sub_sum(root: TreeNode, sum: int):
             if root is None:
-                return
-            set_copy = []
-            for n in num_set:
-                if n == root.val:
+                return []
+            could_sum_to = []
+            left_sub_sum = sub_sum(root.left, sum)
+            right_sub_sum = sub_sum(root.right, sum)
+            if root.val == sum:
+                self.cnt += 1
+            for i in left_sub_sum+right_sub_sum:
+                if i == sum - root.val:
                     self.cnt += 1
-                set_copy.append(n - root.val)
-            set_copy.append(total)
-            canSum(root.left, set_copy, total)
-            canSum(root.right, set_copy, total)
-        canSum(root, [sum], sum)
+                could_sum_to.append(root.val+i)
+            could_sum_to.append(root.val)
+            return could_sum_to
+        sub_sum(root, sum)
         return self.cnt
