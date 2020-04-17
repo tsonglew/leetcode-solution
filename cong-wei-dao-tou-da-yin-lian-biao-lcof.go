d1b3fe715e41d7e8dd86d79234abfea206b79dee
klew <kasheemlew@mails.ccnu.edu.cn>
2020-04-17 09:59:15 +0800

    Create cong-wei-dao-tou-da-yin-lian-biao-lcof.go

---
+++ cong-wei-dao-tou-da-yin-lian-biao-lcof.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reversePrint(head *ListNode) []int {
+    stack := []int{}
+    for head != nil {
+        stack = append(stack, head.Val)
+        head = head.Next
+    }
+    for i, j := 0, len(stack)-1; i < j; i, j = i+1, j-1 {
+        stack[i], stack[j] = stack[j], stack[i]
+    }
+    return stack
+}
