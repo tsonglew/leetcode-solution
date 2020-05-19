cb192daf49198e1cabd9ea1b903f6288cb7dd512
klew <417879751@qq.com>
2020-05-19 23:07:56 +0800

    updated 33

--- 33.search-in-rotated-sorted-array/main.go
+++ 33.search-in-rotated-sorted-array/main.go
@@ -1,52 +1,23 @@
-package main
-
 func search(nums []int, target int) int {
-	length := len(nums)
-	if length < 1 {
-		return -1
-	}
-	lo := 0
-	hi := length - 1
-	var mid int
-	for lo < hi {
-		mid = (lo + hi) / 2
-		if nums[mid] > nums[hi] {
-			lo = mid + 1
-		} else {
-			hi = mid
-		}
-	}
-	hi = (lo - 1 + length) % length
-	lf := binarySearch(nums, 0, hi, target)
-	rt := binarySearch(nums, lo, length-1, target)
-	if lf != -1 {
-		return lf
-	}
-	return rt
+    return subSearch(nums, 0, len(nums)-1, target)
 }
 
-func binarySearch(nums []int, start, end, target int) int {
-	mid := (start + end) / 2
-	for start < end {
-		mid = (start + end) / 2
-		if nums[mid] < target {
-			start = mid + 1
-		} else if nums[mid] > target {
-			end = mid - 1
-		} else {
-			break
-		}
-	}
-	if start == end {
-		if nums[start] != target {
-			return -1
-		}
-		return start
-	}
-	if nums[mid] == target {
-		return mid
-	}
-	return -1
+func subSearch(nums []int, lo, hi int, target int) int {
+    if lo > hi {
+        return -1
+    }
+    mid := (lo+hi)/2
+    if nums[mid] == target {
+        return mid
+    }
+    if nums[lo] <= nums[mid] {
+        if nums[lo] <= target && target <= nums[mid] {
+            return subSearch(nums, lo, mid-1, target)
+        }
+        return subSearch(nums, mid+1, hi, target)
+    }
+    if nums[mid] <= target && target <= nums[hi] {
+        return subSearch(nums, mid+1, hi, target)
+    }
+    return subSearch(nums, lo, mid-1, target)
 }
-
-func main() {}
