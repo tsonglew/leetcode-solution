501a2d173dc3e84774b64325d194b867df322d8b
klew <417879751@qq.com>
2020-05-20 21:19:45 +0800

    updated 42

--- 42.trapping-rain-water/dp.go
+++ 42.trapping-rain-water/dp.go
@@ -1,35 +1,30 @@
-package main
-
-import (
-	"fmt"
-	"math"
-	"time"
-)
-
 func trap(height []int) int {
-	if len(height) <= 2 {
-		return 0
-	}
-	leftMax := make([]int, len(height))
-	rightMax := make([]int, len(height))
-	leftMax[0] = height[0]
-	rightMax[len(height)-1] = height[len(height)-1]
-
-	for i := 1; i < len(height); i++ {
-		leftMax[i] = int(math.Max(float64(leftMax[i-1]), float64(height[i])))
-		ri := len(height) - i - 1
-		rightMax[ri] = int(math.Max(float64(rightMax[ri+1]), float64(height[ri])))
-	}
-	total := 0
-	for i := range leftMax {
-		toAdd := int(math.Min(float64(leftMax[i]), float64(rightMax[i]))) - height[i]
-		total += toAdd
-	}
-	return total
+    maxLeft := make([]int, len(height))
+    maxRight := make([]int, len(height))
+    currentMxLeft, currentMxRight := 0, 0
+    for i := range height {
+        maxLeft[i] = currentMxLeft
+        maxRight[len(height)-1-i] = currentMxRight
+        currentMxLeft = max(currentMxLeft, height[i])
+        currentMxRight = max(currentMxRight, height[len(height)-1-i])
+    }
+    ans := 0
+    for i := range height {
+        ans += max(min(maxLeft[i], maxRight[i])-height[i], 0)
+    }
+    return ans
 }
 
-func main() {
-	start := time.Now()
-	fmt.Println(trap([]int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}))
-	fmt.Printf("costed: %v", time.Since(start))
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
 }
