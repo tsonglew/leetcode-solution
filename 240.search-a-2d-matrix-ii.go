e9ce4d1b4d129ef6e0cae8b309d1db0c7fcfbfee
klew <417879751@qq.com>
2020-05-27 20:52:33 +0800

    updated 240

--- 240.search-a-2d-matrix-ii.go
+++ 240.search-a-2d-matrix-ii.go
@@ -1,24 +1,19 @@
 func searchMatrix(matrix [][]int, target int) bool {
-    if len(matrix) == 0 || len(matrix[0]) == 0 {
-        return false
+    if len(matrix) == 0 || len(matrix[0]) == 0 { return false }
+    for i := range matrix {
+        if matrix[i][len(matrix[0])-1] < target {
+            continue
+        }
+        if matrix[i][0] > target {
+            return false
+        }
+        for j := range matrix[i] {
+            if matrix[i][j] == target {
+                return true
+            } else if matrix[i][j] > target {
+                break
+            }
+        }
     }
-    return searchTarget(matrix, 0, len(matrix)-1, 0, len(matrix[0])-1, target)
-}
-
-func searchTarget(matrix [][]int, rowL, rowH, colL, colH, target int) bool {
-    if rowL > rowH || colL > colH {
-        return false
-    }
-    midRow := int((rowL + rowH) / 2)
-    midCol := int((colL + colH) / 2)
-    mid := matrix[midRow][midCol]
-    if mid == target {
-        return true
-    } else if mid > target {
-        return searchTarget(matrix, rowL, midRow-1, colL, colH, target) ||
-        searchTarget(matrix, rowL, rowH, colL, midCol-1, target)
-    } else {
-        return searchTarget(matrix, midRow+1, rowH, colL, colH, target) ||
-        searchTarget(matrix, rowL, midRow, midCol+1, colH, target)
-    }
+    return false
 }
