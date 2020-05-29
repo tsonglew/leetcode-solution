d3f37eb84e716c0b18f758223c28abad6e416c61
klew <417879751@qq.com>
2020-05-29 09:14:11 +0800

    updated 198

--- 198.house-robber.go
+++ 198.house-robber.go
@@ -1,27 +1,17 @@
 func rob(nums []int) int {
-	switch len(nums) {
-	case 0:
-		return 0
-	case 1:
-		return nums[0]
-	case 2:
-		return max(nums[1], nums[0])
-	case 3:
-		return max(nums[1], nums[0]+nums[2])
-	}
-	nums[2] = nums[0] + nums[2]
-	for i := 3; i < len(nums); i++ {
-		nums[i] = max(nums[i]+nums[i-2], nums[i]+nums[i-3])
-	}
-	return max(nums[len(nums)-3:]...)
-}
-
-func max(nums ...int) int {
-	mx := nums[0]
-	for i := range nums {
-		if nums[i] > mx {
-			mx = nums[i]
-		}
-	}
-	return mx
+    ans := 0
+    for i := range nums {
+        cur := nums[i]
+        if i >= 2 && nums[i-2] + nums[i] > cur {
+            cur = nums[i-2] + nums[i]
+        }
+        if i >= 3 && nums[i-3] + nums[i] > cur {
+            cur = nums[i-3] + nums[i]
+        }
+        nums[i] = cur
+        if nums[i] > ans {
+            ans = nums[i]
+        }
+    }
+    return ans
 }
