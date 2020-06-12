bb76d8693b145c7db2220ef23a6db164d489e735
klew <417879751@qq.com>
2020-06-13 02:30:05 +0800

    update 70

--- 70.climbing-stairs/main.go
+++ 70.climbing-stairs/main.go
@@ -1,18 +1,8 @@
-package main
-
-func main() {
-
-}
-
 func climbStairs(n int) int {
-	if n < 3 {
-		steps := []int{0, 1, 2, 3}
-		return steps[n]
-	}
-	steps := make([]int, n+1)
-	steps[0], steps[1], steps[2] = 0, 1, 2
-	for i := 3; i <= n; i++ {
-		steps[i] = steps[i-1] + steps[i-2]
-	}
-	return steps[n]
+    if n <= 2 { return n }
+    a, b := 1, 2
+    for i := 3; i <= n; i++ {
+        a, b = b, a+b
+    }
+    return b
 }
