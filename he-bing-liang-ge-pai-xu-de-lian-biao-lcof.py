26c47ebde5ee53e2226e4e76d65e996e6511850b
Tsonglew <klew1879@gmail.com>
2021-07-30 14:29:27 +0800

    Create he-bing-liang-ge-pai-xu-de-lian-biao-lcof.py

---
+++ he-bing-liang-ge-pai-xu-de-lian-biao-lcof.py
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode(0)
+        p = head
+        while l1 or l2:
+            if l1 and l2 and l1.val <= l2.val:
+                p.next, l1 = l1, l1.next
+            else:
+                p.next, l2 = l2, l2.next
+            p = p.next
+        return head.next
