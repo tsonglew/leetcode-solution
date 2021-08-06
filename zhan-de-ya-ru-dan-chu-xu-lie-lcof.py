5baa2add5798c0b27394b8bba1b182fb012d7b2e
Tsonglew <klew1879@gmail.com>
2021-08-06 13:39:50 +0800

    Update zhan-de-ya-ru-dan-chu-xu-lie-lcof.py

--- zhan-de-ya-ru-dan-chu-xu-lie-lcof.py
+++ zhan-de-ya-ru-dan-chu-xu-lie-lcof.py
@@ -1,21 +1,11 @@
 class Solution:
     def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        if len(pushed) != len(popped):
-            return False
-        num_index_map = {}
-        for idx, num in enumerate(pushed):
-            num_index_map[num] = idx
-        popped = [num_index_map[num] for num in popped]
-        return self.validate(popped)
-
-    def validate(self, l):
-        if len(l) <= 1:
-            return True
-        less = l[0]
-        for i in l[1:]:
-            if i < l[0]:
-                if i > less:
-                    return False
-                less = i
-        return self.validate(l[1:])
+        l = []
+        poped_i = 0
+        for i in pushed:
+            l.append(i)
+            while len(l) > 0 and poped_i < len(popped) and l[-1] == popped[poped_i]:
+                l.pop()
+                poped_i += 1
+        return len(l) == 0
             
