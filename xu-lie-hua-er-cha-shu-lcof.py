70a4a27144b96c78873d861f0b5d6de3dc6ba4e9
Tsonglew <klew1879@gmail.com>
2021-08-23 20:41:40 +0800

    Update xu-lie-hua-er-cha-shu-lcof.py

--- xu-lie-hua-er-cha-shu-lcof.py
+++ xu-lie-hua-er-cha-shu-lcof.py
@@ -13,33 +13,12 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        r = {}
-        self.nums = defaultdict(lambda: 0)
-        r['pre'] = self.preorder(root)
-        self.nums = defaultdict(lambda: 0)
-        r['in'] = self.inorder(root)
-        r = json.dumps(r)
-        return r
-        
+        return json.dumps(self.preorder(root))    
 
     def preorder(self, root):
         if not root:
-            return []
-        v = root.val
-        self.nums[root.val] += 1
-        if self.nums[root.val] > 1:
-            v = f"{root.val}|{self.nums[root.val]}"
-        return [v, *self.preorder(root.left), *self.preorder(root.right)]
-    
-    def inorder(self, root):
-        if not root:
-            return []
-        v = root.val
-        self.nums[root.val] += 1
-        if self.nums[root.val] > 1:
-            v = f"{root.val}|{self.nums[root.val]}"
-        return [*self.inorder(root.left), v, *self.inorder(root.right)]
-
+            return [None]
+        return [root.val, *self.preorder(root.left), *self.preorder(root.right)]
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -47,19 +26,22 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        inorder = json.loads(data)['in']
-        preorder = json.loads(data)['pre']
-        return self.generate(inorder, preorder)
-
-    def generate(self, inorder, preorder):
-        if len(inorder) == 0:
-            return None
-        v = preorder[0]
-        inorder_idx = inorder.index(v)
-        n = TreeNode(v) if isinstance(v, int) else TreeNode(int(v.split('|')[0]))
-        n.left = self.generate(inorder[:inorder_idx], preorder[1:inorder_idx+1])
-        n.right = self.generate(inorder[inorder_idx+1:], preorder[inorder_idx+1:])
+        self.i = 0
+        self.data = json.loads(data)
+        return self.generate()
+    
+    def generate(self):
+        if self.i >= len(self.data):
+            return
+        if self.data[self.i] is None:
+            self.i += 1
+            return
+        n = TreeNode(self.data[self.i])
+        self.i += 1
+        n.left = self.generate()
+        n.right = self.generate()
         return n
+        
         
 
 # Your Codec object will be instantiated and called as such:
