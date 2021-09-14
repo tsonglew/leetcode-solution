d21cdb8aae42395688fcbdaace09bc00b87c43b6
Tsonglew <klew1879@gmail.com>
2021-09-14 23:47:30 +0800

    Update shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.py

--- shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.py
+++ shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof.py
@@ -1,23 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        bits = [0]*32
+        ones = 0
+        twos = 0
         for n in nums:
-            for i in self.bit(n):
-                print(i)
-                bits[i] += 1
-        for i, _ in enumerate(bits):
-            bits[i] %= 3
-        i = 0
-        r = 0
-        while i < 32:
-            r += bits[i] * pow(2, i)
-            i += 1
-        return r
-
-    def bit(self, n):
-        r = 0
-        while n:
-            if n % 2 == 1:
-                yield r
-            r += 1
-            n >>= 1
+            twos, ones = (twos&~n)|(~twos&n&ones), ~twos&(ones^n)
+        return ones
