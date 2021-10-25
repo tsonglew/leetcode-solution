a1659622c9632b58a43f66cbed5d9505aa97d5c8
Tsonglew <klew1879@gmail.com>
2021-10-25 21:58:27 +0800

    Update 240.search-a-2d-matrix-ii.go

--- 240.search-a-2d-matrix-ii.go
+++ 240.search-a-2d-matrix-ii.go
@@ -17,3 +17,24 @@ func searchMatrix(matrix [][]int, target int) bool {
     }
     return false
 }
+
+func searchMatrixMethod2(matrix [][]int, target int) bool {
+    if len(matrix) > 0 {
+        return f(matrix, 0, len(matrix)-1, 0, len(matrix[0])-1, target)
+    }
+    return false
+}
+
+func f(matrix [][]int, i1, i2, j1, j2, target int) bool {
+    if i1 > i2 || j1 > j2 || i1 < 0 || i2 >= len(matrix) || j1 < 0 || j2 > len(matrix[0]) {
+        return false
+    }
+    mid_i, mid_j := (i1+i2)/2, (j1+j2)/2
+    mid := matrix[mid_i][mid_j]
+    if mid == target {
+        return true
+    } else if mid < target {
+        return f(matrix, mid_i+1, i2, j1, j2, target) || f(matrix, i1, mid_i, mid_j+1, j2, target)
+    }
+    return f(matrix, i1, mid_i-1, j1, j2, target) || f(matrix, mid_i, i2, j1, mid_j-1, target)
+}
