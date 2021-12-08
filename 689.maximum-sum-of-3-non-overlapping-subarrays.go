e7e301ddeabd0f6cdcbcdc7036accf3310cbc28a
Tsonglew <klew1879@gmail.com>
2021-12-09 00:58:44 +0800

    Update 689.maximum-sum-of-3-non-overlapping-subarrays.go

--- 689.maximum-sum-of-3-non-overlapping-subarrays.go
+++ 689.maximum-sum-of-3-non-overlapping-subarrays.go
@@ -1,41 +1,21 @@
-func maxSumOfThreeSubarrays(nums []int, k int) []int {
-    array1Index := 0
-    array2IndexStart, _ := getArray23Index(array1Index, k)
-    array1Sum, array2Sum, array3Sum := 0, 0, 0
-    array1Max, array2Max, array3Max := 0, 0, 0
-    maxSum := 0
-    result := []int{0, 0, 0}
-
-    array1MaxStart, array2MaxStart, array3MaxStart := -1, -1, -1
-    for ;array1Index <= len(nums) - 3 * k; array1Index++ {
-        array2Index, array3Index := getArray23Index(array1Index, k)
-        array1Sum += nums[array1Index]
-        array2Sum += nums[array2Index]
-        array3Sum += nums[array3Index]
-
-        if array1Index < array2IndexStart - 1 {
-            continue
+func maxSumOfTwoSubarrays(nums []int, k int) (ans []int) {
+    var sum1, maxSum1, maxSum1Idx int
+    var sum2, maxSum12 int
+    for i := k; i < len(nums); i++ {
+        sum1 += nums[i-k]
+        sum2 += nums[i]
+        if i >= k*2-1 {
+            if sum1 > maxSum1 {
+                maxSum1 = sum1
+                maxSum1Idx = i - k*2 + 1
+            }
+            if maxSum1+sum2 > maxSum12 {
+                maxSum12 = maxSum1 + sum2
+                ans = []int{maxSum1Idx, i - k + 1}
+            }
+            sum1 -= nums[i-k*2+1]
+            sum2 -= nums[i-k+1]
         }
-
-        if array1Sum > array1Max {
-            array1Max = array1Sum
-            array1MaxStart = array1Index-k+1
-        }
-        if array2Sum > array2Max {
-            array2Max = array2Sum
-            array2MaxStart = array2Index-k+1
-        }
-        if array3Sum > array3Max {
-            array3Max = array3Sum
-            array3MaxStart = array3Index-k+1
-        }
-        if array1Sum + array2Sum + array3Sum > maxSum {
-            maxSum = array1Sum + array2Sum + array3Sum
-            result = []int{array1MaxStart, array2MaxStart, array3MaxStart}
-        }
-        array1Sum -= nums[array1Index-k+1]
-        array2Sum -= nums[array2Index-k+1]
-        array3Sum -= nums[array3Index-k+1]
     }
-    return result
+    return
 }
