8c042a4117dfe382800f0623ee403154e1c250e2
Tsong Lew <tsonglew@gmail.com>
2024-11-20 22:49:24 +0800

    feat: init 994

---
+++ 994.rotting-oranges/src/main.rs
@@ -0,0 +1,13 @@
+struct Solution;
+
+impl Solution {
+    pub fn oranges_rotting(grid: Vec<Vec<i32>>) -> i32 {
+        let mut m: Vec<Vec<i32>> = Vec::new();
+        // for i in 0..grid.len()
+        0
+    }
+}
+
+fn main() {
+    assert!(Solution::oranges_rotting(vec![vec![2, 1, 1], vec![1, 1, 0], vec![0, 1, 1]]) == 4);
+}
