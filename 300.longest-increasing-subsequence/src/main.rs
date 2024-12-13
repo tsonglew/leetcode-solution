760072818954b0f3869457d3b04495933e811fb3
Tsong Lew <tsonglew@gmail.com>
2024-12-14 01:30:42 +0800

    update 300

--- 300.longest-increasing-subsequence/src/main.rs
+++ 300.longest-increasing-subsequence/src/main.rs
@@ -1,21 +1,17 @@
 impl Solution {
     pub fn length_of_lis(nums: Vec<i32>) -> i32 {
-        let mut mem = vec![0; nums.len()];
-        mem[0] = 1;
-        let mut res = 1;
-        for i in 1..nums.len() {
-            let mut x = 1;
+        let mut dp = vec![1; nums.len()];
+        let mut max_v = 0;
+        for i in 0..nums.len() {
+            let mut max_len = 1;
             for j in 0..i {
-                if nums[j] < nums[i] && mem[j] + 1 > x {
-                    x = mem[j] + 1;
+                if nums[i] > nums[j] {
+                    max_len = std::cmp::max(max_len, dp[j] + 1);
                 }
             }
-            mem[i] = x;
-            if mem[i] > res {
-                res = mem[i];
-            }
+            dp[i] = max_len;
+            max_v = std::cmp::max(max_v, max_len);
         }
-        println!("{:?}", mem);
-        res
+        max_v
     }
 }
