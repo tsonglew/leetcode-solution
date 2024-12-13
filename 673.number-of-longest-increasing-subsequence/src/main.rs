4a19819828f484e41010a91c115280fe0d31d745
Tsong Lew <tsonglew@gmail.com>
2024-12-13 23:56:24 +0800

    feat: init 673

--- 673.number-of-longest-increasing-subsequence/src/main.rs
+++ 673.number-of-longest-increasing-subsequence/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+impl Solution {
+    pub fn find_number_of_lis(nums: Vec<i32>) -> i32 {
+        let mut dp = vec![vec![0; nums.len()]];
+        for i in 0..dp.len() {
+            let mut mx = nums[i];
+            for j in i..dp.len() {
+                if nums[j] >= mx {
+                    
+                }
+            }
+        }
+    }
 }
