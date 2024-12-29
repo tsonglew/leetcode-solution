7f97ddf9e4ba5193baef09da2ec96a8446fa70cb
Tsong Lew <tsonglew@gmail.com>
2024-12-30 00:15:31 +0800

    update 206

--- 206.reverse-linked-list/src/main.rs
+++ 206.reverse-linked-list/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+// Definition for singly-linked list.
+// #[derive(PartialEq, Eq, Clone, Debug)]
+// pub struct ListNode {
+//   pub val: i32,
+//   pub next: Option<Box<ListNode>>
+// }
+// 
+// impl ListNode {
+//   #[inline]
+//   fn new(val: i32) -> Self {
+//     ListNode {
+//       next: None,
+//       val
+//     }
+//   }
+// }
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        if head.is_none() {
+            return head;
+        }
+        let mut head_node = head.unwrap();
+        let mut nex: Option<Box<ListNode>> = head_node.next.take();
+        let mut cur: Option<Box<ListNode>> = Some(head_node);
+
+        while !nex.is_none() {
+            let mut nex_node = nex.unwrap();
+            let nn = nex_node.next.take();
+            nex_node.next = cur;
+            cur = Some(nex_node);
+            nex = nn;
+        }
+        return cur;
+    }
 }
