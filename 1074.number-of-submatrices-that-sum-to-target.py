805794f66a9933ecf2e3be3a2fc2de6d2e6529ad
Tsong Lew <tsonglew@gmail.com>
2025-03-03 02:12:13 +0800

    1074

--- 1074.number-of-submatrices-that-sum-to-target.py
+++ 1074.number-of-submatrices-that-sum-to-target.py
@@ -1,30 +1,16 @@
 class Solution:
     def numSubmatrixSumTarget(self, matrix: List[List[int]], target: int) -> int:
-        def subarraySum(nums: List[int], k: int) -> int:
-            mp = Counter([0])
-            count = pre = 0
-            for x in nums:
-                pre += x
-                if pre - k in mp:
-                    count += mp[pre - k]
-                mp[pre] += 1
-            return count
-        
-        m, n = len(matrix), len(matrix[0])
-        ans = 0
-        # 枚举上边界
-        for i in range(m):
-            total = [0] * n
-            # 枚举下边界
-            for j in range(i, m):
-                for c in range(n):
-                    # 更新每列的元素和
-                    total[c] += matrix[j][c]
-                ans += subarraySum(total, target)
-        
-        return ans
-
-作者：力扣官方题解
-链接：https://leetcode.cn/problems/number-of-submatrices-that-sum-to-target/solutions/798061/yuan-su-he-wei-mu-biao-zhi-de-zi-ju-zhen-8ym2/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+        res = 0
+        for row1 in range(len(matrix)):
+            temp = [0] * len(matrix[0])
+            for row2 in range(row1, len(matrix)):
+                m = {0: 1}
+                preSum = 0
+                for col in range(len(matrix[0])):
+                    # print("***(%d,0),(%d,%d)"%(row1,row2,col))
+                    temp[col] += matrix[row2][col]
+                    preSum += temp[col]
+                    res += m.get(preSum - target, 0)
+                    m[preSum] = m.get(preSum, 0) + 1
+                    # print(res, temp, m)
+        return res
