90850d5aebb0e2dd77b1fd72d31127ff27bf2123
Tsonglew <tsonglew@gmail.com>
2025-03-31 00:05:42 +0800

    Create 911.online-election.java

---
+++ 911.online-election.java
@@ -0,0 +1,38 @@
+class TopVotedCandidate {
+
+    int[] top;
+    int[] times;
+
+    public TopVotedCandidate(int[] persons, int[] times) {
+        this.times = times;
+        this.top = new int[persons.length];
+        HashMap<Integer, Integer> topMap = new HashMap<>();
+        Integer topPerson = -1;
+        for (int i = 0; i < persons.length; i++) {
+            int person = persons[i];
+            topMap.computeIfAbsent(person, k -> 0);
+            topMap.put(person, topMap.get(person) + 1);
+            if (topPerson < 0 || topMap.get(person) >= topMap.get(topPerson)) {
+                topPerson = person;
+            }
+            this.top[i] = topPerson;
+        }
+    }
+    
+    public int q(int t) {
+        int l = 0, r = this.times.length - 1;
+        int ans = 0;
+        while (l <= r) {
+            int mid = (int) Math.floor((l + r) / 2);
+            if (this.times[mid] == t) {
+                return this.top[mid];
+            } else if (this.times[mid] > t) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+                ans = Math.max(ans, mid);
+            }
+        }
+        return this.top[ans];
+
+    }
