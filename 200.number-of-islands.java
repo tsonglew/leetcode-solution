b9c2eb6c7d2303cf2c6302aac98312733fb040fb
Tsonglew <tsonglew@gmail.com>
2025-04-08 00:17:31 +0800

    Update 200.number-of-islands.java

--- 200.number-of-islands.java
+++ 200.number-of-islands.java
@@ -12,13 +12,13 @@ class Solution {
 
     public int numIslands(char[][] grid) {
         Queue<Pair> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[grid.length+2][grid[0].length+2];
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
         int ans = 0;
         int[] dx = new int[]{0, 1, 0, -1};
-        int[] dy = new int[]{-1, 0, 1, 1};
+        int[] dy = new int[]{-1, 0, 1, 0};
         
         for (int i = 0; i < visited.length; i++) {
-            for (int j = 0; j < visited[0].lenght; j++) {
+            for (int j = 0; j < visited[0].length; j++) {
                 if (visited[i][j]) {
                     continue;
                 }
@@ -26,24 +26,22 @@ class Solution {
                     visited[i][j] = true;
                     continue;
                 }
-
                 ans ++;
-                System.out.printf("%d %d\n", i, j);
                 queue.add(new Pair(i, j));
                 while (!queue.isEmpty()) {
                     Pair p = queue.poll();
                     if (visited[p.l][p.r]) {
                         continue;
                     }
-                    visited[p.l][p.r] = true;                    
-                    if (grid[p.l][p.r] != '1' && p.l != -1 && p.l != grid.length && p.r != -1 && p.r != grid[0].length) {
+                    if (grid[p.l][p.r] != '1') {
                         continue;
                     }
+                    visited[p.l][p.r] = true;
                     for (int di = 0; di < 4; di++) {
                         int nl = p.l + dx[di];
                         int nr = p.r + dy[di];
 
-                        if (nl >= -1 && nl <= grid.length && nr >= -1 && nr <= grid[0].length) {
+                        if (nl >= 0 && nl < grid.length && nr >= 0 && nr < grid[0].length) {
                             queue.add(new Pair(nl, nr));
                         }
                     }
@@ -54,3 +52,4 @@ class Solution {
 
     }
 }
+
