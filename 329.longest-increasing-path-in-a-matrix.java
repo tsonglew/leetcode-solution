459cc40674cc302e6cf916d0f78316e3c32826fc
Tsonglew <tsonglew@gmail.com>
2025-04-15 00:57:42 +0800

    Update 329.longest-increasing-path-in-a-matrix.java

--- 329.longest-increasing-path-in-a-matrix.java
+++ 329.longest-increasing-path-in-a-matrix.java
@@ -1,75 +1,71 @@
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Queue;
-
 class Solution {
-
-    static class Pair {
-        public Integer left;
-        public Integer right;
-
-        public Pair(Integer left, Integer right) {
-            this.left = left;
-            this.right = right;
-        }
-    }
-
     int m;
     int n;
     int[][] direction = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
     HashMap<Integer, ArrayList<Integer>> to;
     HashMap<Integer, Integer> dist;
+    int ans;
+    int[] indegree;
+    Queue<Integer> q;
 
     private void init(int[][] matrix) {
         this.m = matrix.length;
         this.n = matrix[0].length;
         this.to = new HashMap<>(this.m * this.n);
         this.dist = new HashMap<>(this.m * this.n);
+        this.ans = 1;
+        this.indegree = new int[this.m * this.n];
+        this.q = new LinkedList<>();
     }
 
     public int longestIncreasingPath(int[][] matrix) {
         this.init(matrix);
         this.setTopo(matrix);
-
-        for (Map.Entry<Integer, ArrayList<Integer>> e : this.to.entrySet()) {
-            if (e.getValue().isEmpty()) {
-                continue;
+        
+        for (int i = 0; i < this.indegree.length; i++) {
+            if (this.indegree[i] == 0) {
+                this.dist.putIfAbsent(i, 1);
+                this.q.add(i);
+                
             }
-
         }
+        this.bfs();
+        return this.ans;
     }
 
-    private void bfs(Integer hashij) {
-        int dist = 1;
+    private void bfs() {
+        while (!this.q.isEmpty()) {
+            Integer startNode = this.q.poll();
+            this.ans = Math.max(this.ans, this.dist.get(startNode));
+            ArrayList<Integer> toList = this.to.get(startNode);
+            if (toList == null) {
+                continue;
+            }
+            for (Integer to : toList) {
+                this.dist.put(to, Math.max(this.dist.getOrDefault(to, 1), this.dist.get(startNode)+1));
+                this.indegree[to]--;
+                if (this.indegree[to] == 0) {
+                    q.add(to);
+                }
 
-        this.dist.put(hashij, Math.max(dist, this.dist.getOrDefault(hashij, 0)));
-        Queue<Pair> q = new ArrayList<>();
-        q.add(new Pair(hashij, this.dist.get(hashij)));
-        while (!q.isEmpty()) {
-            Pair p = q.poll();
-            int hashd = p.left;
-            int dist = p.right;
-            int i = (int) Math.floor(hashij / this.m);
-            int j = hashij % this.m;
-
+            }
         }
-
     }
 
     private void setTopo(int[][] matrix) {
         for (int i = 0; i < this.m; i++) {
             for (int j = 0; j < this.n; j++) {
+                int hashij = this.hash(i, j);
+                this.to.put(hashij, new ArrayList<Integer>());
                 for (int[] d : this.direction) {
                     int next_i = i + d[0], next_j = j + d[1];
                     if (!this.valid(next_i, next_j)) {
                         continue;
                     }
-                    int hashij = this.hash(i, j);
-                    this.to.putIfAbsent(hashij, k -> new ArrayList<>());
                     if (matrix[next_i][next_j] > matrix[i][j]) {
                         this.to.get(hashij).add(this.hash(next_i, next_j));
+                    } else if (matrix[next_i][next_j] < matrix[i][j]) {
+                        this.indegree[hashij]++;
                     }
                 }
 
@@ -79,10 +75,17 @@ class Solution {
     }
 
     private int hash(int i, int j) {
-        return this.m * i + j;
+        return this.n * i + j;
     }
 
     private boolean valid(int i, int j) {
         return i >= 0 && i < this.m && j >= 0 && j < this.n;
+    }
+
+    private void iterList(List<Integer> l) {
+        for (Integer i : l) {
+            System.out.printf("%d ", i);
+        }
+        System.out.println();
     }
 }
