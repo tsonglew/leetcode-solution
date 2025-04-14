26c5de1f1eb376e33de63c76d07fc61cd7428744
Tsonglew <tsonglew@gmail.com>
2025-04-14 21:32:34 +0800

    Update 329.longest-increasing-path-in-a-matrix.java

--- 329.longest-increasing-path-in-a-matrix.java
+++ 329.longest-increasing-path-in-a-matrix.java
@@ -1,78 +1,88 @@
-class Solution {
-    private int m;
-    private int n;
-    private HashMap<Integer, ArrayList<Integer>> to;
-    private int[] dx;
-    private int[] dy;
-    private int[] degree;
-    private int ans;
-    private int[] dist;
 
-    public int longestIncreasingPath(int[][] matrix) {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
+class Solution {
+
+    static class Pair {
+        public Integer left;
+        public Integer right;
+
+        public Pair(Integer left, Integer right) {
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    int m;
+    int n;
+    int[][] direction = { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+    HashMap<Integer, ArrayList<Integer>> to;
+    HashMap<Integer, Integer> dist;
+
+    private void init(int[][] matrix) {
         this.m = matrix.length;
         this.n = matrix[0].length;
-        this.to = new HashMap<>();
-        this.dx = new int[]{0, 0, -1, 1};
-        this.dy = new int[]{-1, 1, 0, 0};
-        this.degree = new int[this.m*this.n];
-        this.ans = 1;
-        this.dist = new int[this.m*this.n];
-        this.setUpTopo(matrix);
-        // System.out.println("finish setupt topo");
-        this.bfs();
-        return this.ans;
+        this.to = new HashMap<>(this.m * this.n);
+        this.dist = new HashMap<>(this.m * this.n);
+    }
+
+    public int longestIncreasingPath(int[][] matrix) {
+        this.init(matrix);
+        this.setTopo(matrix);
+
+        for (Map.Entry<Integer, ArrayList<Integer>> e : this.to.entrySet()) {
+            if (e.getValue().isEmpty()) {
+                continue;
+            }
+
+        }
+    }
+
+    private void bfs(Integer hashij) {
+        int dist = 1;
+
+        this.dist.put(hashij, Math.max(dist, this.dist.getOrDefault(hashij, 0)));
+        Queue<Pair> q = new ArrayList<>();
+        q.add(new Pair(hashij, this.dist.get(hashij)));
+        while (!q.isEmpty()) {
+            Pair p = q.poll();
+            int hashd = p.left;
+            int dist = p.right;
+            int i = (int) Math.floor(hashij / this.m);
+            int j = hashij % this.m;
+
+        }
+
+    }
+
+    private void setTopo(int[][] matrix) {
+        for (int i = 0; i < this.m; i++) {
+            for (int j = 0; j < this.n; j++) {
+                for (int[] d : this.direction) {
+                    int next_i = i + d[0], next_j = j + d[1];
+                    if (!this.valid(next_i, next_j)) {
+                        continue;
+                    }
+                    int hashij = this.hash(i, j);
+                    this.to.putIfAbsent(hashij, k -> new ArrayList<>());
+                    if (matrix[next_i][next_j] > matrix[i][j]) {
+                        this.to.get(hashij).add(this.hash(next_i, next_j));
+                    }
+                }
+
+            }
+        }
+
     }
 
     private int hash(int i, int j) {
-        return i * this.m + j;
+        return this.m * i + j;
     }
 
     private boolean valid(int i, int j) {
         return i >= 0 && i < this.m && j >= 0 && j < this.n;
-    }
-
-    private void setUpTopo(int[][] matrix) {
-        for (int i = 0; i < this.m; i++) {
-            for (int j = 0; j < this.n; j++) {
-                int hashij = this.hash(i, j);
-                this.to.put(hashij, new ArrayList<>());
-                for (int d = 0; d < this.dx.length; d++) {
-                    int ni = i + dx[d];
-                    int nj = j + dy[d];
-                    if (!this.valid(ni, nj)) {
-                        continue;
-                    }
-                    if (matrix[ni][nj] > matrix[i][j]) {
-                        this.to.get(hashij).add(this.hash(ni, nj));
-                        this.degree[hashij] ++;
-                    }
-                }  
-            }
-        }
-    }
-
-    private void bfs() {
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < this.degree.length; i++) {
-            if (this.degree[i] == 0) {
-                continue;
-            }
-            q.add(this.degree[i]);
-            int sum = 1;
-            while (!q.isEmpty()) {
-                Integer ele = q.poll();
-                // System.out.printf("ele: %d\n", ele);
-                this.dist[ele] = Math.max(this.dist[ele], sum + 1);
-                this.ans = Math.max(this.dist[ele], this.ans);
-                int row = (int) Math.floor(ele / this.m);
-                int col = ele % this.m;
-                for (Integer nexthash: this.to.get(ele)) {
-                    q.add(nexthash);
-                }                
-                sum++;
-                
-            }
-        }
     }
 }
